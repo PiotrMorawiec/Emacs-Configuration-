afeed731715055713e1993af75e1ccaa75cb1478
pmorawie <piotr.morawiec@nokia.com>
2021-10-14 22:48:13 +0200

    Fix on Helm buffers occupying entire buffer when treemacs enabled | use XREF with Helm | some org mode config

--- lisp-custom-libs/custom-functions.el
+++ lisp-custom-libs/custom-functions.el
@@ -1,24 +1,24 @@
 
 
 (defun scroll-half-page-down ()
-	"scroll down half the page"
-	(interactive)
-	(scroll-down (/ (window-body-height) 2)))
+        "scroll down half the page"
+        (interactive)
+        (scroll-down (/ (window-body-height) 2)))
 
 (defun scroll-half-page-up ()
-	"scroll up half the page"
-	(interactive)
-	(scroll-up (/ (window-body-height) 2)))
+        "scroll up half the page"
+        (interactive)
+        (scroll-up (/ (window-body-height) 2)))
 
 (defvar sel-region "")
 (defun toggle-highlight-region-duplicates ()
-	"Function toggles higlight of all occurances of currently selected region"
-	(interactive)
-	(if (and (use-region-p) (> (region-end) (+ (region-beginning) 1)))
-	    (progn
-	      (setq sel-region (buffer-substring (region-beginning) (region-end)))
-	      (highlight-regexp sel-region 'region))
-	  (unhighlight-regexp t)))
+        "Function toggles higlight of all occurances of currently selected region"
+        (interactive)
+        (if (and (use-region-p) (> (region-end) (+ (region-beginning) 1)))
+            (progn
+              (setq sel-region (buffer-substring (region-beginning) (region-end)))
+              (highlight-regexp sel-region 'region))
+          (unhighlight-regexp t)))
 
 
 (defun duplicate-current-line-or-region (arg)
@@ -40,6 +40,15 @@ there's a region, all lines that region covers will be duplicated."
         (insert region)
         (setq end) (point))
       (goto-char (+ origin (* (length region) arg) arg)))))
+
+(defun toggle-highlight-trailing-whitespaces ()
+  "Function toggles highlighting trailing whitespaces"
+  (interactive)
+  (if (= show-trailing-whitespace 1)
+      (progn  (message "Disable highlighting of trailing whitespaces")
+              (setq-default show-trailing-whitespace nil))
+  (progn (message "Enable highlighting of trailing whitespaces")
+         (setq-default show-trailing-whitespace 1))))
 
 (defun which-active-modes ()
   "Give a message of which minor modes are enabled in the current buffer."
@@ -66,7 +75,7 @@ there's a region, all lines that region covers will be duplicated."
 (defun my-org-mode-setup ()
   (interactive)
   (org-indent-mode)
-  (variable-pitch-mode 1)
+  (variable-pitch-mode 1) ;; < what is that ?
   ;; Enable text wrapping in org-mode (it looks better when side piddings enbaled)
   (visual-line-mode 1))
 
@@ -96,5 +105,17 @@ there's a region, all lines that region covers will be duplicated."
   (setq org-agenda-files
   '(;;"~/projects/pusch//org/placeholder.org"
     "~/programming/org/tutorial/tips.org")))
+
+(defun my-untabify-entire-buffer ()
+  (interactive)
+  (mark-whole-buffer)
+  (untabify (region-beginning) (region-end))
+  (message "Converting all TAB's to spaces")
+  (keyboard-quit))
+
+(defun my-open-init-file ()
+  (interactive)
+  (find-file "~/.emacs.d/init.el")
+  (message "Init file opened"))
 
 (provide 'custom-functions)
